8637560076e4b4a136f16d1ef3655ed6537772d6
noriyotcp <noriyo.akita@gmail.com>
2021-09-26 19:47:01 +0900

    Config webpack for renderer and monaco workers
    compiled output to .webpack/renderer

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -2,17 +2,32 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  },
+  {
+    test: /\.ttf$/,
+    use: ["file-loader"],
+  }
+);
 
 module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/renderer.ts",
+    "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+    "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
+    "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
+    "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
+    "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker",
+  },
   module: {
     rules,
   },
   plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
   },
 };

bff880521e79dfb0c26fa78d54fb6d7f08f636b8
noriyotcp <noriyo.akita@gmail.com>
2021-09-26 19:08:26 +0900

    Disable Electron Security Warnings

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { app, BrowserWindow } from 'electron';
 // whether you're running in development or production).
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "1";
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();

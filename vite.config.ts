2599e4d636880a6290ba21d6c6b6a6733a9b9718
noriyotcp <noriyo.akita@gmail.com>
2021-10-10 02:08:45 +0900

    Remove Vue.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.ELECTRON == "true" ? "./" : ".",
-  plugins: [vue()],
   build: {
     rollupOptions: {
       input: {

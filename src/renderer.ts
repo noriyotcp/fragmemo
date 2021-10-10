1b9c65c10dfb541b575a4eca2f70f559e9d76552
noriyotcp <noriyo.akita@gmail.com>
2021-10-10 21:13:32 +0900

    Add homeElement

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,35 +1,34 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 const { myAPI } = window;
+let homeElement: ShadowRoot;
 
 document.addEventListener("DOMContentLoaded", (): void => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const textarea = document.querySelector<HTMLTextAreaElement>("#text")!;
+  homeElement = document.getElementsByTagName("home-element")[0].shadowRoot!;
+  const textarea = homeElement.querySelector<HTMLTextAreaElement>("#text")!;
   myAPI.setupStorage().then((msg: string) => {
     textarea.value = msg;
   });
+
+  homeElement
+    .querySelector<HTMLButtonElement>("#btn-save")!
+    .addEventListener("click", () => {
+      const textarea = homeElement.querySelector<HTMLTextAreaElement>("#text")!;
+      myAPI.fileSaveAs(textarea.value);
+    });
+
+  const openByMenuListener = (fileData: any) => {
+    if (fileData.status === undefined) {
+      return false;
+    }
+
+    if (!fileData.status) {
+      alert(`ファイルが開けませんでした\n${fileData.message}`);
+      return false;
+    }
+
+    const textarea = homeElement.querySelector<HTMLTextAreaElement>("#text")!;
+    textarea.value = fileData.text;
+  };
 });
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-document
-  .querySelector<HTMLButtonElement>("#btn-save")!
-  .addEventListener("click", () => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const textarea = document.querySelector<HTMLTextAreaElement>("#text")!;
-    myAPI.fileSaveAs(textarea.value);
-  });
-
-const openByMenuListener = (fileData: any) => {
-  if (fileData.status === undefined) {
-    return false;
-  }
-
-  if (!fileData.status) {
-    alert(`ファイルが開けませんでした\n${fileData.message}`);
-    return false;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const textarea = document.querySelector<HTMLTextAreaElement>("#text")!;
-  textarea.value = fileData.text;
-};
 
 myAPI.openByMenu((_e: Event, fileData: object) => openByMenuListener(fileData));

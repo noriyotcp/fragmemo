4e7a5f479fd5fe076d8cd2c64127dae4ae98707a
noriyotcp <noriyo.akita@gmail.com>
2021-10-17 12:35:02 +0900

    Remove homeElement

--- src/components.ts
+++ src/components.ts
@@ -2,5 +2,4 @@ import "./components/app-element";
 import "./components/settings-element";
 import "./components/test-header";
 import "./components/codeEditor";
-// import "./components/homeElement";
 import "./components/snippet-list";

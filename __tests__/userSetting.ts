970caa5310e8fca6522ca9f2fe1cf7c03ea4d88f
noriyotcp <noriyo.akita@gmail.com>
2021-11-03 19:37:31 +0900

    Add UserSetting
    pass app.getPath("userData") to constructor
    
    Add readSettings()
    if a settings.json does not exist, create it with default settings
    
    Add defaultSettings variable
    
    Add test against UserSetting

---
+++ __tests__/userSetting.ts
@@ -0,0 +1,66 @@
+import fs from "fs";
+import path from "path";
+import os from "os";
+import UserSetting from "../main-process/userSetting";
+
+describe("new UserSetting()", () => {
+  const userSetting = new UserSetting(
+    "/Users/user_name/Library/Application Support/fragmemo"
+  );
+  it("should an instance of UserSetting and returns appPath", () => {
+    expect(userSetting).toBeInstanceOf(UserSetting);
+    expect(userSetting.appPath).toBe(
+      "/Users/user_name/Library/Application Support/fragmemo"
+    );
+  });
+});
+
+describe("readSettings()", () => {
+  let tmpDir: string;
+  beforeEach(() => {
+    try {
+      tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "test-"));
+    } catch (err) {
+      throw new Error("Could not create tmp dir");
+    }
+  });
+  afterEach(() => {
+    try {
+      if (tmpDir) {
+        fs.rmSync(tmpDir, { recursive: true });
+      }
+    } catch (e) {
+      console.error(
+        `An error has occurred while removing the temp folder at ${tmpDir}. Please remove it manually. Error: ${e}`
+      );
+    }
+  });
+
+  describe("Settings file exists", () => {
+    it("should return the settings", () => {
+      fs.writeFileSync(
+        `${tmpDir}/settings.json`,
+        JSON.stringify({
+          theme: "custom",
+        })
+      );
+      expect(new UserSetting(tmpDir).readSettings()).toEqual({
+        theme: "custom",
+      });
+    });
+  });
+
+  describe("Settings file does not exists", () => {
+    it("should return the default settings", () => {
+      expect(new UserSetting(tmpDir).readSettings()).toEqual({
+        theme: "light",
+      });
+    });
+  });
+});
+
+// TODO: test settings file is empty
+// describe("Settings file is empty", () => {
+//   it("", () => {
+//   });
+// });

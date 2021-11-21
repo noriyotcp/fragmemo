0c55a04c70da0ff64e9a73a90853a964fdfe68aa
noriyotcp <noriyo.akita@gmail.com>
2021-11-21 23:12:31 +0900

    Add db/db.ts (DB class) and export Realm schema

---
+++ main-process/db/db.ts
@@ -0,0 +1,49 @@
+import { Realm, realmSchema } from "./realm";
+
+class DB extends Realm {
+  constructor(path: string) {
+    super({ path, schema: realmSchema });
+  }
+
+  reverseSortById(className: string): Realm.Results<Realm.Object> {
+    return this.objects(className).sorted("_id", true);
+  }
+
+  currentMaxId(className: string): number {
+    return <number>this.objects(className).max("_id") || 0;
+  }
+
+  createSnippet(title: string): void {
+    // For testing
+    const testContentOfFragment = `var num: number = 123;
+function identity(num: number): number {
+    return num;
+}`;
+
+    this.createFragment("", testContentOfFragment);
+    const latestFragment = this.objectForPrimaryKey(
+      "Fragment",
+      this.currentMaxId("Fragment")
+    );
+
+    this.write(() => {
+      this.create("Snippet", {
+        _id: this.currentMaxId("Snippet") + 1,
+        title: title,
+        fragments: [latestFragment],
+      });
+    });
+  }
+
+  createFragment(title: string, content: string): void {
+    this.write(() => {
+      this.create("Fragment", {
+        _id: this.currentMaxId("Fragment") + 1,
+        title: title,
+        content: content,
+      });
+    });
+  }
+}
+
+export default DB;

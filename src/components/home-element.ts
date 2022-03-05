78251b92d7bb93519bc901204fec7f1b4ac3b148
noriyotcp <noriyo.akita@gmail.com>
2022-03-05 17:00:52 +0900

    Cleanup home-element
    Do not switch templates any more

--- src/components/home-element.ts
+++ src/components/home-element.ts
@@ -1,12 +1,8 @@
 import { LitElement, html, css, TemplateResult } from "lit";
-import { customElement, state } from "lit/decorators.js";
-const { myAPI } = window;
+import { customElement } from "lit/decorators.js";
 
 @customElement("home-element")
 export class HomeElement extends LitElement {
-  @state()
-  private _current_page = "home-element";
-
   static styles = [
     css`
       :host {
@@ -20,28 +16,10 @@ export class HomeElement extends LitElement {
     `,
   ];
 
-  toastTemplate() {
-    return html`<toast-element></toast-element>`;
-  }
-
   render(): TemplateResult {
-    if (this._current_page == "home-element") {
-      return html`
-        ${this.toastTemplate()}
-        <editor-element></editor-element>
-      `;
-    } else {
-      return html`
-        ${this.toastTemplate()}
-        <settings-element
-          @back-to-home=${this._backToHomeListener}
-        ></settings-element>
-      `;
-    }
-  }
-
-  private _backToHomeListener(e: CustomEvent) {
-    console.log(e.detail.elementName);
-    this._current_page = e.detail.elementName;
+    return html`
+      <toast-element></toast-element>
+      <editor-element></editor-element>
+    `;
   }
 }

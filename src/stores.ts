adb8c7f2217d85782ef056553bfb9f81d9981a15
noriyotcp <noriyo.akita@gmail.com>
2022-03-07 23:39:22 +0900

    Remove BaseStore

--- src/stores.ts
+++ src/stores.ts
@@ -1,39 +1,5 @@
 const { createStore, Store } = TinyBase;
 
-class BaseStore {
-  store: typeof Store;
-  readonly TABLE_NAME = "states";
-  schema = {};
-
-  constructor() {
-    this.store = createStore().setSchema(this.schema);
-  }
-
-  hasRow(rowId: string) {
-    return this.store.hasRow(this.TABLE_NAME, `${rowId}`);
-  }
-
-  setRow(rowId: string, cells: object) {
-    this.store.setRow(this.TABLE_NAME, `${rowId}`, cells);
-  }
-
-  setPartialRow(rowId: string, cells: object) {
-    this.store.setPartialRow(this.TABLE_NAME, `${rowId}`, cells);
-  }
-
-  getRow(rowId: string) {
-    return this.store.getRow(this.TABLE_NAME, `${rowId}`);
-  }
-
-  getCell(rowId: string, cell: string) {
-    return this.store.getCell(this.TABLE_NAME, `${rowId}`, cell);
-  }
-
-  setCell(rowId: string, cell: string, value: string | boolean | number) {
-    this.store.setCell(this.TABLE_NAME, `${rowId}`, cell, value);
-  }
-}
-
 const createFragmentStore = (): typeof Store => {
   const schema = {
     states: {

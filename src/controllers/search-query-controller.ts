575f525b001d5a93a5841fb733b3ab67bef3255c
noriyotcp <noriyo.akita@gmail.com>
2022-03-28 23:57:49 +0900

    Rename event handler
    it's not a listener

--- src/controllers/search-query-controller.ts
+++ src/controllers/search-query-controller.ts
@@ -10,7 +10,7 @@ export class SearchQueryController implements ReactiveController {
   hostConnected(): void {
     window.addEventListener(
       "search-snippets",
-      this._searchSnippetsListener as EventListener
+      this._searchSnippets as EventListener
     );
     window.addEventListener(
       "clear-internal-search-query",
@@ -18,7 +18,7 @@ export class SearchQueryController implements ReactiveController {
     );
   }
 
-  private _searchSnippetsListener = (e: CustomEvent) => {
+  private _searchSnippets = (e: CustomEvent) => {
     this.query = e.detail.query;
   };
 

9d79c5078b2de1d244ebc8fab1a655fff26dd1b1
noriyotcp <noriyo.akita@gmail.com>
2022-04-30 21:29:24 +0900

    Set `/` to vite base option
    the following warning message had shown:
     (!) invalid "base" option: .. The value can only be an absolute URL, ./, or an empty string.
    
    cf. https://vitejs.dev/config/#base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.ELECTRON == "true" ? "./" : ".",
+  base: process.env.ELECTRON == "true" ? "./" : "/",
   build: {
     rollupOptions: {
       input: {

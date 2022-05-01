a4c3a1ab1fc9b76252ffa162e8afcb8ce9119997
noriyotcp <noriyo.akita@gmail.com>
2022-05-02 01:09:28 +0900

    Add createDefaultSettings to initJsonStorage.ts
    import and use it in window.ts

--- main-process/fragmemoSettings/initJsonStorage.ts
+++ main-process/fragmemoSettings/initJsonStorage.ts
@@ -1,4 +1,6 @@
 import { app } from "electron";
+import { setTimeout } from "timers/promises";
+import fs from "node:fs";
 import path from "node:path";
 import { JsonStorage, DatapathDoesNotExistError } from "../jsonStorage";
 
@@ -18,5 +20,27 @@ const initJsonStorage = async (filename: string): Promise<JsonStorage> => {
   }
 };
 
-export { initJsonStorage, pathToRestore };
+const createDefaultSettings = async (
+  settings: object,
+  filename: string
+): Promise<void> => {
+  try {
+    fs.mkdirSync(pathToRestore, { recursive: true });
+    // Use fs.writeFileSync instead of electron-json-storage set()
+    // electron-json-storage set() is async, so we need to wait for it to finish
+    // github.dev/electron-userland/electron-json-storage/blob/df4edce1e643e7343d962721fe2eacfeda094870/lib/storage.js#L419-L439
+    fs.writeFileSync(
+      path.resolve(pathToRestore, filename),
+      JSON.stringify(settings, null, 2)
+    );
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    // But, just in case, we'll wait for 1 millisecond :)
+    await setTimeout(1);
+  }
+};
+
+export { initJsonStorage, pathToRestore, createDefaultSettings };
 export { JsonStorage, DatapathDoesNotExistError } from "../jsonStorage";
--- main-process/fragmemoSettings/window.ts
+++ main-process/fragmemoSettings/window.ts
@@ -1,9 +1,6 @@
-import { setTimeout } from "timers/promises";
-import path from "node:path";
-import fs from "node:fs";
 import {
   initJsonStorage,
-  pathToRestore,
+  createDefaultSettings,
   JsonStorage,
   DatapathDoesNotExistError,
 } from "./initJsonStorage";
@@ -15,34 +12,15 @@ const initRestoreWindowStorage = async (): Promise<JsonStorage> => {
     })
     .catch(async (error) => {
       if (error instanceof DatapathDoesNotExistError) {
-        await createDefaultWindowSettings();
+        const defaultWindowSettings = {
+          window: { width: 800, height: 600, x: 0, y: 0 },
+        };
+        await createDefaultSettings(defaultWindowSettings, "window.json");
         return initJsonStorage("window.json");
       } else {
         return Promise.reject(error);
       }
     });
-};
-
-const createDefaultWindowSettings = async (): Promise<void> => {
-  try {
-    fs.mkdirSync(pathToRestore, { recursive: true });
-    const defaultWindowSettings = {
-      window: { width: 800, height: 600, x: 0, y: 0 },
-    };
-    // Use fs.writeFileSync instead of electron-json-storage set()
-    // electron-json-storage set() is async, so we need to wait for it to finish
-    // github.dev/electron-userland/electron-json-storage/blob/df4edce1e643e7343d962721fe2eacfeda094870/lib/storage.js#L419-L439
-    fs.writeFileSync(
-      path.resolve(pathToRestore, "window.json"),
-      JSON.stringify(defaultWindowSettings, null, 2)
-    );
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    // But, just in case, we'll wait for 1 millisecond :)
-    await setTimeout(1);
-  }
 };
 
 type WindowDataType = {

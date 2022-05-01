91961e47c34074494e072bdf392871ad4d273796
noriyotcp <noriyo.akita@gmail.com>
2022-05-01 21:19:21 +0900

    Extract createDefaultWindowSettings()

--- main-process/settings/restore/window.ts
+++ main-process/settings/restore/window.ts
@@ -11,21 +11,30 @@ const initRestoreWindowStorage = async (): Promise<JsonStorage> => {
     return Promise.resolve(new JsonStorage(pathToRestore));
   } catch (error) {
     if (error instanceof DatapathDoesNotExistError) {
-      fs.mkdirSync(pathToRestore, { recursive: true });
-      const defaultWindowSettings = {
-        window: { width: 800, height: 600, x: 0, y: 0 },
-      };
-      // Use fs.writeFileSync instead of electron-json-storage set()
-      // electron-json-storage set() is async, so we need to wait for it to finish
-      // github.dev/electron-userland/electron-json-storage/blob/df4edce1e643e7343d962721fe2eacfeda094870/lib/storage.js#L419-L439
-      fs.writeFileSync(
-        path.resolve(pathToRestore, "window.json"),
-        JSON.stringify(defaultWindowSettings, null, 2)
-      );
+      await createDefaultWindowSettings();
       return Promise.resolve(new JsonStorage(pathToRestore));
     } else {
       return Promise.reject(error);
     }
+  }
+};
+
+const createDefaultWindowSettings = async (): Promise<void> => {
+  try {
+    fs.mkdirSync(pathToRestore, { recursive: true });
+    const defaultWindowSettings = {
+      window: { width: 800, height: 600, x: 0, y: 0 },
+    };
+    // Use fs.writeFileSync instead of electron-json-storage set()
+    // electron-json-storage set() is async, so we need to wait for it to finish
+    // github.dev/electron-userland/electron-json-storage/blob/df4edce1e643e7343d962721fe2eacfeda094870/lib/storage.js#L419-L439
+    fs.writeFileSync(
+      path.resolve(pathToRestore, "window.json"),
+      JSON.stringify(defaultWindowSettings, null, 2)
+    );
+  } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     // But, just in case, we'll wait for 1 millisecond :)
     await setTimeout(1);

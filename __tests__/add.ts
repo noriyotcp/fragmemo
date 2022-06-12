5221f29b66432c047d9341b92b6916b0b2717d33
noriyotcp <noriyo.akita@gmail.com>
2022-06-12 21:22:54 +0900

    Remove obsolete tests

--- __tests__/add.ts
+++
@@ -1,8 +0,0 @@
-import { add } from "../main-process/add";
-
-describe("add", () => {
-  it("should return 2 when it gives 1,1", () => {
-    const result = add(1, 1);
-    expect(result).toBe(2);
-  });
-});
